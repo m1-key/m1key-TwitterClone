596be06bca98401654331cecc175ab66e805a828
Mithilesh Tiwari <shubhamtiwari.tiwari84@gmail.com>
2020-10-19 20:17:30 +0530

    Sidebar First Stage Completed

--- src/SideBar/SidebarOption.js
+++ src/SideBar/SidebarOption.js
@@ -3,7 +3,7 @@ import "./SidebarOption.css"
 
 function SidebarOption(props) {
   return (
-    <div className="sidebarOption">
+    <div className={`sidebarOption ${props.active && 'sidebarOption--active'}`}>
         <props.Icon></props.Icon>
       <h2>{props.text} 😃 </h2>
     </div>
--- src/SideBar/index.js
+++ src/SideBar/index.js
@@ -9,12 +9,15 @@ import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
+
+import "./index.css"
 
 function SideBar() {
   return (
     <div className="sidebar">
       <TwitterIcon />
-      <SidebarOption Icon={HomeIcon} text={"Home"} />
+      <SidebarOption active Icon={HomeIcon} text={"Home"} />
       <SidebarOption Icon={SearchIcon} text={"Explore"} />
       <SidebarOption Icon={NotificationsNoneIcon} text={"Notifications"} />
       <SidebarOption Icon={MailOutlineIcon} text="Messages" />
@@ -22,6 +25,7 @@ function SideBar() {
       <SidebarOption Icon={ListAltIcon} text="Lists" />
       <SidebarOption Icon={PermIdentityIcon} text="Profile" />
       <SidebarOption Icon={MoreHorizIcon} text="More" />
+      <Button variant="outlined" className="sidebar--tweet" fullWidth>Tweet</Button>
     </div>
   );
 }
